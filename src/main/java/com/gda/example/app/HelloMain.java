b81c09b9b63beb0889029fbb2bea97eda792b7bb
krystianluc <krystian.luc@q-perior.com>
2025-03-24 07:30:39 +0100

    feat: example feature

--- src/main/java/com/gda/example/app/HelloMain.java
+++ src/main/java/com/gda/example/app/HelloMain.java
@@ -7,6 +7,7 @@ public class HelloMain {
     public static String defaultPort = "8080";
 
     public static void main(String[] args) {
+
         int port = Integer.parseInt(System.getProperty("port", defaultPort));
         MyServer httpServer = new MyServer();
         httpServer.Start(port);
